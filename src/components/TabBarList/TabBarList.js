710a9992a8fd26f22122eb4f5009a18cc28bdaad
Angel Fuerte <harrysonfuerte@gmail.com>
2018-04-24 13:18:50 -0500

    Se agrego un button de ejemplo

--- src/components/TabBarList/TabBarList.js
+++ src/components/TabBarList/TabBarList.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react'
 import * as action from '../../actions'
 import {connect} from 'react-redux'
 
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+
 class TabBarList extends Component{
 	
 	handleClick(e){
@@ -14,9 +16,13 @@ class TabBarList extends Component{
 		return (
 			<div className="TabBarList">
 				<h1>TabBarList</h1>
-					<button id="tab_1" Onclick= { this.handleClick } >TAB_1</button>
-					<button id="tab_2" Onclick= { this.handleClick } >TAB_2</button>
-					<button id="tab_3" Onclick= { this.handleClick } >TAB_3</button>
+				<h1> Modificaciones en Git </h1>
+					<Button color="success"  id="tab_1" Onclick= { this.handleClick } >TAB_1</Button>
+					<Button color="success" id="tab_2" Onclick= { this.handleClick } >TAB_2</Button>
+					<Button color="success" id="tab_3" Onclick= { this.handleClick } >TAB_3</Button>
+
+					<h1>Commit en la rama test</h1>
+					<Button color="success" id="tab_3" Onclick= { this.handleClick } >Hola</Button>
 			</div>
 		)
 
